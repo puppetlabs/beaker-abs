c78b266ab45677fb8bfdcc5f8c36a3e5a0faba37
Josh Cooper <josh@puppetlabs.com>
2016-10-26 17:38:21 -0700

    (maint) Test that version is a triple dotted number
    The test was broken, update the test to assert that the version string
    is a triple dotted version number instead of not being nil.

--- test/beaker-abs_test.rb
+++ test/beaker-abs_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
+require 'beaker-abs/version'
 
 class Beaker::AbsTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::BeakerAbs::VERSION
+    assert_match(/\d+\.\d+\.\d+/, BeakerAbs::Version::STRING)
   end
 end

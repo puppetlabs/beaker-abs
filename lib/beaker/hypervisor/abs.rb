f9d74a6251d1ea184b9d0162311fe6a28ba60171
Josh Cooper <josh@puppetlabs.com>
2016-10-26 17:48:20 -0700

    (QENG-4466) Tag vmpooler VMs
    This copies the logic from beaker's vmpooler hypervisor to tag VMs based
    on global options, host-specific options, and the current beaker
    version. It also adds a cinext=true tag.

--- lib/beaker/hypervisor/abs.rb
+++ lib/beaker/hypervisor/abs.rb
@@ -1,4 +1,5 @@
 require 'beaker'
+require 'beaker/version'
 require 'json'
 
 module Beaker
@@ -22,28 +23,83 @@ module Beaker
       #   "type"     => "centos-7-i386",
       #   "engine"   => "vmpooler",
       # }
-      @resource_hosts.each do |resource_host|
-        type = resource_host['type']
-        type2hosts[type] ||= []
-        type2hosts[type] << resource_host['hostname']
-      end
+      type2hosts = @resource_hosts.group_by { |resource_host| resource_host['type'] }
+
+      missing_templates = []
+      missing_hosts = []
 
       # for each host, get a vm for that template type
       @hosts.each do |host|
         template = host['template']
 
-        raise ArgumentError.new("Failed to provision host '#{host.hostname}' because its 'template' is missing.") if template.nil?
+        if template.nil?
+          missing_templates << host
+          next
+        end
 
         if provisioned_hosts = type2hosts[template]
-          host['vmhostname'] = provisioned_hosts.shift
+          host['vmhostname'] = provisioned_hosts.shift['hostname']
+          tag(host)
         else
-          raise ArgumentError.new("Failed to provision host '#{host.hostname}', no template of type '#{host['template']}' was provided.")
+          missing_hosts << host
+        end
+      end
+
+      if host = missing_templates.first
+        raise ArgumentError.new("Failed to provision host '#{host.hostname}' because its 'template' is missing.")
+      end
+
+      if host = missing_hosts.first
+        raise ArgumentError.new("Failed to provision host '#{host.hostname}', no template of type '#{host['template']}' was provided.")
+      end
+
+      type2hosts.each_pair do |_, resource_hosts|
+        if host = resource_hosts.first
+          raise ArgumentError.new("unexpected host '#{host['hostname']}' of type '#{host['type']}' was provided")
         end
       end
     end
 
     def cleanup
       # nothing to do
+    end
+
+    private
+
+    def tag(h)
+      begin
+        uri = URI.parse(@options[:pooling_api] + '/vm/' + h['vmhostname'].split('.')[0])
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Put.new(uri.request_uri)
+
+        # merge pre-defined tags with host tags
+        request.body = { 'tags' => add_tags(h) }.to_json
+
+        response = http.request(request)
+        parsed_response = JSON.parse(response.body)
+
+        unless parsed_response['ok']
+          @logger.notify "Failed to tag host '#{h['vmhostname']}'!"
+        end
+      rescue JSON::ParserError => e
+        @logger.notify "Failed to tag host '#{h['vmhostname']}'! (failed with #{e.class})"
+      rescue => e
+        @logger.notify "Failed to connect to vmpooler for tagging!: #{e.inspect}"
+      end
+    end
+
+    def add_tags(host)
+      (host[:host_tags] || {}).merge(
+        'beaker_version'    => Beaker::Version::STRING,
+        'jenkins_build_url' => @options[:jenkins_build_url],
+        'department'        => @options[:department],
+        'project'           => @options[:project],
+        'created_by'        => @options[:created_by],
+        'name'              => host.name,
+        'roles'             => host.host_hash[:roles].join(', '),
+        'cinext'            => 'true'
+      )
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,12 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'beaker-abs'
 require 'beaker'
 
+require "logger"
 require 'minitest/autorun'
+require 'webmock/minitest'
+
+WebMock.disable_net_connect!
+
+def create_logger(io)
+  Beaker::Logger.new(io, :quiet => true)
+end

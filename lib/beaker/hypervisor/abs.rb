887c198e8e15fed466700c199661f828b4b9491b
Christopher Thorn <christopher.thorn@puppetlabs.com>
2020-08-17 08:20:26 -0700

    (BKR-1679) Provision VM/NSPooler with ABS
    This PR introduces a way to provision with vmfloaty VMs and NSPooler
    hosts, if the ABS_RESOURCE_HOSTS is not set and provision=true.
    Most likely dev workflow will end up needing this.

--- lib/beaker/hypervisor/abs.rb
+++ lib/beaker/hypervisor/abs.rb
@@ -1,5 +1,6 @@
 require 'beaker'
 require 'json'
+require 'pry-byebug'
 
 module Beaker
   class Abs < Beaker::Hypervisor
@@ -9,7 +10,9 @@ module Beaker
       @hosts = hosts
 
       resource_hosts = ENV['ABS_RESOURCE_HOSTS'] || @options[:abs_resource_hosts]
-      raise ArgumentError.new("ABS_RESOURCE_HOSTS must be specified when using the Beaker::Abs hypervisor") if resource_hosts.nil?
+
+      raise ArgumentError.new("ABS_RESOURCE_HOSTS must be specified when using the Beaker::Abs hypervisor when provisioning") if resource_hosts.nil? && !options[:provision]
+      resource_hosts = provision_vms(hosts).to_json if resource_hosts.nil?
       @resource_hosts = JSON.parse(resource_hosts)
     end
 
@@ -73,6 +76,66 @@ module Beaker
 
     def cleanup
       # nothing to do
+    end
+
+    def provision_vms(hosts)
+      ns_request, vm_request = generate_floaty_request_strings(hosts)
+      # TODO Implement ns_request
+
+      vm_beaker_abs = []
+      # HACKY NEED TO PROPERLY ACCESS VMFLOATY
+      # Will return a JSON Object like this:
+      # {"redhat-7-x86_64"=>["rich-apparition.delivery.puppetlabs.net", "despondent-side.delivery.puppetlabs.net"], "centos-7-x86_64"=>["firmer-vamp.delivery.puppetlabs.net"]}
+      vm_floaty_output = JSON.parse(`floaty --service=abs get #{vm_request} --json`)
+
+      vm_floaty_output.each do |os_platform, value|
+        value.each do | hostname |
+          vm_beaker_abs.push({"hostname": hostname, "type": os_platform, "engine":"vmpooler"})
+        end
+      end
+
+      # to do need to figure out what the NSPooler requests are, and combine the output for that with vm_beaker_abs and return that object
+      # for now just returning vmpooler abs object
+      vm_beaker_abs
+    end
+
+    # Based upon the host file, this method generates two strings for what we need to pass to floaty
+    # in order to generate nspooler and vmpooler requests
+    def generate_floaty_request_strings(hosts)
+      # Hopefully clean this up at some point?
+      # Use the vmfloaty gem methods directly to determine this?
+      # Need to swallow errors if person isn't setup to use nspooler, since most people wont need to
+      hacky_ns_list = `floaty list --service=ns`.split
+      hacky_vm_list = `floaty list --service=vmpooler`.split
+      ns_list = {}
+      vm_list = {}
+      hosts.each do |host|
+        if hacky_vm_list.include?(host[:template])
+          if vm_list.include?(host[:template])
+            vm_list[host[:template]] = vm_list[host[:template]] + 1
+          else
+            vm_list[host[:template]] = 1
+          end
+        elsif hacky_ns_list.include?(host[:template])
+          if ns_list.include?(host[:template])
+	    ns_list[host[:template]] = ns_list[host[:template]] + 1
+          else
+            ns_list[host[:template]] = 1
+          end
+        else
+          raise ArgumentError.new("#{host.name} has a template #{host[:template]} that is not found in vmpooler or nspooler")
+        end
+      end
+      vm_request = ""
+      ns_request = ""
+      vm_list.each do |key, value|
+        vm_request.concat("#{key}=#{value} ")
+      end
+      ns_list.each do |key, value|
+	ns_request.concat("#{key}=#{value} ")
+      end
+
+      return ns_request, vm_request
     end
   end
 end
